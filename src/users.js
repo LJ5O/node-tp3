0b7836e650dcf61e82ac743acb39b1bb40bdbc7d
LJ5O <75009579+LJ5O@users.noreply.github.com>
2023-03-10 10:53:52 +0100

    Ajout de la fonction getPhoneNumbersByStartingAge

--- src/users.js
+++ src/users.js
@@ -4,4 +4,25 @@ function age(users) {
   return users.filter((user) => user.age >= 50);
 }
 
-console.log(age(usersData));
+
+/**
+* @function getPhoneNumbersByStartingAge
+* @param  {number} age L'âge minimal à partir duquel récupérer les numéros de téléphone
+* @returns {array<string>} Liste de tous les numéros trouvés
+*/
+function getPhoneNumbersByStartingAge(age){
+  foundPhoneNumbers = [];
+
+  for(let i=0;i<usersData.length;i++){
+    //Boucle sur tous les users
+
+    if(usersData[i].age >= age){
+      //S'il a l'âge minimal de la recherche
+      foundPhoneNumbers.push(usersData[i].phone);
+    }
+  }
+  return foundPhoneNumbers;
+}
+
+//console.log(age(usersData));
+console.log(getPhoneNumbersByStartingAge(50));
